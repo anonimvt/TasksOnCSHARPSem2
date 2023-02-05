7bb53ab10059ab2b4155ccec1188025fd2fc5d6e
anonimvt <rinat.madyarov.08@gmail.com>
2023-02-05 18:16:21 +0400

    Seminar2Task10

---
+++ task10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace NewProgram;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число");
+        while(true)
+        {
+            string? input = Console.ReadLine();
+            if(!input.Equals("exit"))
+                Console.WriteLine(input[1]);
+        }
+
+    }
+}
