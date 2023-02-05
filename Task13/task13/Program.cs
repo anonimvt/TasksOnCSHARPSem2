319f91412a71cc6aa0ff0dd4242d318a0c701d86
anonimvt <75793665+anonimvt@users.noreply.github.com>
2023-02-05 18:27:50 +0400

    Tasks13

---
+++ Task13/task13/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace NewProgram;
+
+public class Program
+{
+    public static void Main()
+    {   
+        Console.WriteLine("Введите число");
+        while(true)
+        {
+            string? input = Console.ReadLine();
+            if(input.Length > 2)
+                Console.WriteLine(input[2]);
+            else
+            {
+                Console.WriteLine("Третий цифры нет.");
+            }
+        }
+    }
+}
