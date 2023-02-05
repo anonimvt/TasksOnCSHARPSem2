88cc7e4a9d95a43fffb225ca0016560327b038e1
anonimvt <75793665+anonimvt@users.noreply.github.com>
2023-02-05 18:35:52 +0400

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace NewProgram;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Введите число");
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input == 6 || input == 7)
+
+        {
+            Console.WriteLine("Да");   
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+
+    }
+}
